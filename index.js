3012a0ab4e6be70f5de542c3560ba9e20ecd3a6a
Gabriel Alves dos Santos <86124609+GabrielAlSant@users.noreply.github.com>
2022-05-04 21:06:50 +0000

    Fiz Atualizar, deletar e achar o usuario por id.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.urlencoded({
   extended: true
 }))
 
+app.get("/:id", async function (req, res){
+ const resultado =  await usuario.findOne({where : {id:req.params.id}});
+  res.json(resultado)
+  
+});
+
 app.get("/", async function (req, res){
  const resultado =  await usuario.findAll();
   res.json(resultado)
@@ -17,6 +23,17 @@ app.post("/", function (req, res){
  var resultado = usuario.create(req.body);
   res.json(resultado);
 });
+
+app.put("/:id", function (req, res){
+ var resultado =  usuario.update(req.body, {where : {id:req.params.id}});
+ res.json(resultado) 
+});
+
+app.delete("/:id", async function (req, res){
+ var resultado =  await usuario.destroy({where : {id:req.params.id}});
+ res.json(resultado) 
+});
+
 
 app.listen(3000, function(){
   console.log("O servidor esta em killing spree B)")
