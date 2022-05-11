d40410fea10394127ed106793c1bcf73b84b3b1b
Gabriel Alves dos Santos <86124609+GabrielAlSant@users.noreply.github.com>
2022-05-11 21:05:30 +0000

    Adicionei a Empresa e fiz a relação dela com o usuário, fiz o CRUD dela também (com as rotas).

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 var express = require("express");
 var app = express();
 var { usuario } = require("./models")
+var { empresa } = require("./models")
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/:id", async function (req, res){
+app.get("/usuarios/:id", async function (req, res){
  const resultado =  await usuario.findOne({where : {id:req.params.id}});
   res.json(resultado)
   
 });
 
-app.get("/", async function (req, res){
+app.get("/usuarios", async function (req, res){
  const resultado =  await usuario.findAll();
   res.json(resultado)
   
 });
 
-app.post("/", function (req, res){
+app.post("/usuarios", function (req, res){
  var resultado = usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", function (req, res){
+app.put("/usuarios/:id", function (req, res){
  var resultado =  usuario.update(req.body, {where : {id:req.params.id}});
  res.json(resultado) 
 });
 
-app.delete("/:id", async function (req, res){
+app.delete("/usuarios/:id", async function (req, res){
  var resultado =  await usuario.destroy({where : {id:req.params.id}});
  res.json(resultado) 
 });
+
+
+/*Empresas*/
+
+
+app.get("/empresas/:id", async function (req, res){
+ const resultado =  await empresa.findOne({where : {id:req.params.id}});
+  res.json(resultado)
+  
+});
+
+app.get("/empresas", async function (req, res){
+ const resultado =  await empresa.findAll();
+  res.json(resultado)
+  
+});
+
+app.post("/empresas", function (req, res){
+ var resultado = empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", function (req, res){
+ var resultado =  empresa.update(req.body, {where : {id:req.params.id}});
+ res.json(resultado) 
+});
+
+app.delete("/empresas/:id", async function (req, res){
+ var resultado =  await empresa.destroy({where : {id:req.params.id}});
+ res.json(resultado) 
+});
+
 
 
 app.listen(3000, function(){
